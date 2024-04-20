204f69d5aba48812f135aa18827a511ca2a91325
RURIGIMK <rurigimk@gmail.com>
2024-04-20 13:42:01 +0300

    Initial

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === query.toLowerCase();
+      });
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+function matchName(driverObjects, name) {
+    return driverObjects.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
